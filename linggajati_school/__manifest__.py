321f18c89f231c982d020bc46fe70b26faff1cf3
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-02-25 16:13:59 +0700

    add linggajati_school_v13

--- linggajati_school/__manifest__.py
+++ linggajati_school/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "linggajati_shcool",
+    'name': "linggajati_school",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
