1fb5fb09a13d2c136a53915ac123daa10391e2ec
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-02-26 09:50:03 +0700

    fix linggajati_school

--- linggajati_school/__manifest__.py
+++ linggajati_school/__manifest__.py
@@ -3,18 +3,18 @@
     'name': "GNBS",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Customizing School Module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Module for customizing module school (Rename Menu)
+    """,
+
+    'author': "Abu Abdirohman",
+    'website': "http://www.linggajati.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
@@ -25,9 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'views/templates.xml',
-        'views/student.xml',
+        'views/student.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
