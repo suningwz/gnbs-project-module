72ba5e284802cee7803a7c73a0b059d1ad41df1b
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-03-07 09:13:07 +0700

    modif state

--- linggajati_school/models/student.py
+++ linggajati_school/models/student.py
@@ -4,7 +4,15 @@ from odoo import api, fields, models
 class Student(models.Model):
     _inherit = 'student.student'
 
-    # Rename label field
+    # Rename Label Field
     admission_date = fields.Date(string='Register Date')
     standard_id = fields.Many2one(string='Class')
     date_of_birth = fields.Date(string='Date of Birth')
+
+    # Add State
+    state = fields.Selection([('draft', 'Draft'),
+                              ('done', 'Student'),
+                              ('terminate', 'Terminate'),
+                              ('cancel', 'Cancel'),
+                              ('alumni', 'Graduate')],
+                             'Status', readonly=True, default="draft")
