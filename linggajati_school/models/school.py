3ec29b6723b444d277d632a8a6055c9015d0e571
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-03-10 17:13:16 +0700

    Rename Standard -> level form sub menu classes kanban tree

--- linggajati_school/models/school.py
+++ linggajati_school/models/school.py
@@ -7,11 +7,18 @@ class StudentPreviousSchool(models.Model):
     admission_date = fields.Date('Start Date')
     course_id = fields.Many2one(string='Class')
 
-from odoo import api, fields, models
-
-
 class StudentFamilyContact(models.Model):
     _inherit = 'student.family.contact'
 
     relation = fields.Many2one(string='Family Status')
+    
+from odoo import api, fields, models
+
+
+class SchoolStandard(models.Model):
+    _inherit = 'school.standard'
+
+    name = fields.Char(string='Name')
+
+    standard_id = fields.Many2one(string='Level')
     
