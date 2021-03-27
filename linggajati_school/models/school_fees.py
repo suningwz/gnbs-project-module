6a208ec640d8a38553756df86eba96820db0a31b
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-03-27 13:13:38 +0700

    delete folder experiment

--- linggajati_school/models/school_fees.py
+++ linggajati_school/models/school_fees.py
@@ -4,7 +4,4 @@ from odoo import api, fields, models
 class StudentPayslip(models.Model):
     _inherit = 'student.payslip'
 
-    invoiced_count = fields.Integer(
-        string='Sum of Invoiced', compute="_compute_invoiced_count")
-
-    def _compute_invoiced_count(self):
+    
