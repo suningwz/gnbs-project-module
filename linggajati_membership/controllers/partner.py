3b9f187e8396bb685aefad1cf99702e0cfb84e4c
Abu Abdirohman <abuabdirohman4@gmail.com>
2021-04-01 16:52:54 +0700

    group_id succesfully get

--- linggajati_membership/controllers/partner.py
+++ linggajati_membership/controllers/partner.py
@@ -11,18 +11,19 @@ class PartnerForm(http.Controller):
     def customer_form_submit(self, **post):
 
         # Create Partner
-        partner_before = request.env['res.partner'].search([])
-        print('Partner Before Create')
-        print("partner : ", partner_before)
         partner = request.env['res.partner'].create({
             'name': post.get('name'),
             'email': post.get('email'),
         })
-        print('Partner After Create')
+        print('Partner')
+        print("partner : ", partner)
         print("name : ", partner['name'])
         print("id : ", partner['id'])
         print("user id : ", partner['user_id'])
-        print("partner : ", partner)
+
+        parent_grp_id = request.env['ir.model.data'].get_object('school', 'group_school_parent')
+        emp_grp = request.env['ir.model.data'].get_object('base', 'group_user')
+        parent_group_ids = [emp_grp.id, parent_grp_id.id]
 
         # Create user
         user = request.env['res.users'].create({
@@ -30,16 +31,36 @@ class PartnerForm(http.Controller):
             'login' : partner['email'],
             'email' : partner['email'],
             'partner_id' : partner['id'],
+            'groups_id': [(6, 0, parent_group_ids)]
         })
         print('User')
+        print("user : ", user)
         print("name : ", user['name'])
         print("id : ", user['id'])
-        # print("partner id : ", partner['partner_id'])
-        print("user : ", user)
+        print("partner id : ", user['partner_id'])
+        # print("groups_id : ", user['groups_id'])
+        
+        # Experiment Group
+        # parent_grp_id = self.env.ref('school.group_school_parent')
+        parent_grp_id = request.env['ir.model.data'].get_object('school', 'group_school_parent')
+        emp_grp = request.env['ir.model.data'].get_object('base', 'group_user')
+        parent_group_ids = [emp_grp.id, parent_grp_id.id]
+        # emp_grp = self.env.ref('base.group_user')
+        # group_id = request.env['ir.model.data'].get_object('event', 'group_event_user')
+        # group_id.write({'users': [(4, users.id)]})
+        # 'groups_id': [(6, 0, parent_group_ids)]
+        print("parent_grp_id : ", parent_grp_id)
+        print("emp_grp : ", emp_grp)
+        print("parent_group_ids : ", parent_group_ids)
 
-        # user = request.env['res.users'].sudo().search([])
-        # print('User')
-        # print("user : ", user)
+        # Show Values on Table
+        # group = request.env['res.groups'].sudo().search([])
+        # print('Group')
+        # print("group : ", group)
+        # print("group - name : ", request.env['res.groups'].browse('name'))
+        # print("group - name : ", request.env['res.groups']['name'])
+        # group_name = request.env['res.groups']['name']
+        # print("group : ", group_name['Student Parent'])
 
         # Values
         vals = {
